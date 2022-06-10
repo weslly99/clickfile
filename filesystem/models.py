17fd50eae953eb71125b1a7a6988713d13d1075f
Weslly Oliveira <weslly99@hotmail.com>
2022-06-10 14:36:36 -0300

    add directory model

--- filesystem/models.py
+++ filesystem/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Directory(models.Model):
+    name = models.CharField(max_length=200)
+    uri = models.URLField(max_length=255)
+    size = models.IntegerField()
+    parent = models.ForeignKey("filesystem.Directory", verbose_name="parent", on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('name',)
