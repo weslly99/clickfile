deac10febdd05d1a4db4d976f14c2b5b68a5d3f7
Weslly Oliveira <weslly99@hotmail.com>
2022-06-10 14:53:42 -0300

    add file models

--- filesystem/models.py
+++ filesystem/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from .mixins import TimestampableMixin
 
 
-class Directory(models.Model):
+class Directory(TimestampableMixin, models.Model):
     name = models.CharField(max_length=200)
     uri = models.URLField(max_length=255)
     size = models.IntegerField()
-    parent = models.ForeignKey("filesystem.Directory", verbose_name="parent", on_delete=models.CASCADE)
+    parent = models.ForeignKey("filesystem.Directory", verbose_name="parent", on_delete=models.CASCADE, blank=True, null=True)
 
     class Meta:
-        ordering = ('name',)
+        ordering = ('name','updated_at')
+
+
+class File(TimestampableMixin, models.Model):
+    name = models.CharField(max_length=255)
+    mime_type = models.CharField(max_length=50)
+    uri = models.URLField(max_length=255)
+    size = models.IntegerField()
+    directory = models.ForeignKey("filesystem.Directory", verbose_name="directory", on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('name','updated_at')
